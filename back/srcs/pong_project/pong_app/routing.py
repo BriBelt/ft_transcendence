5b9b8ce33f3f06f51a50d1b811193c63922baea1
Javier Sarabia Rodriguez <jsarabia@c3r12s1.42madrid.com>
2024-11-29 18:54:03 +0100

    se crea un web socket con la conexion del user para comprobar si está online o no

--- back/srcs/pong_project/pong_app/routing.py
+++ back/srcs/pong_project/pong_app/routing.py
@@ -5,7 +5,8 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/pong-socket/(?P<userid>\d+)/$', consumers.PongConsumer.as_asgi()), 
-    re_path(r'ws/pong-socket/(?P<tournament>\w+)/(?P<userid>\d+)/$', consumers.PongConsumer.as_asgi()), 
+    re_path(r'ws/pong-socket/(?P<tournament>\w+)/(?P<userid>\d+)/$', consumers.PongConsumer.as_asgi()),
+    re_path(r'ws/online/(?P<userid>\d+)/$', consumers.PlayerConsumer.as_asgi()),
     # 'w+' sirve para crear nuevas rooms por cada llamada mientras que el 
     # '$' sirve para indicar que ahí se acaba la url, si añades algo después no lo va a encontrar
 ]
